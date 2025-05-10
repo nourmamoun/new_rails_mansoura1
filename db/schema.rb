11f3fef13629e7fd329083ca101a43b77c751cae
“[Noor <nourmamoun@gmail.com>
2025-05-10 20:56:08 +0300

    Adding user resources and validations to it

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_10_171416) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_10_175422) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.date "dob"
+    t.string "email", null: false
+    t.string "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "address", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
