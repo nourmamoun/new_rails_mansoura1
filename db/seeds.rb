5b249f619f94430f3f64187db2ed8b1489aed81d
“[Noor <nourmamoun@gmail.com>
2025-05-11 15:34:55 +0300

    Adding seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,34 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Create Users
+users = []
+5.times do |i|
+  users << User.create!(
+    name: "User #{i + 1}",
+    email: "user#{i + 1}@example.com",
+    phone_number: "012345678#{i}",
+    dob: Date.new(1990 + i, 1, 1),
+    address: "Address #{i + 1}"
+  )
+end
+
+# Create Posts with a creator
+posts = []
+3.times do |i|
+  posts << Post.create!(
+    title: "Post #{i + 1}",
+    content: "This is the content of post #{i + 1}.",
+    creator: users.sample
+  )
+end
+
+# Assign random editors to each post
+posts.each do |post|
+  editors = users.sample(2) 
+  editors.each do |editor|
+    Editorship.create!(post: post, user: editor)
+  end
+end
+
